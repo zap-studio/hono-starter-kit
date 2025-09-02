ec85686f587397efffe39a1edaba9e9333811aaf
Alexandre Trotel <trotelalexandre@proton.me>
2025-09-02 19:55:12 +0200

    feat: enhance ApiSuccess type to include optional metadata; update sendJson function for improved type handling

--- src/zap/utils/response.ts
+++ src/zap/utils/response.ts
@@ -4,24 +4,28 @@ import { HttpStatus } from './http';
 
 /**
  * Represents a successful API response.
+ *
+ * @template T - The type of the data returned in the response.
+ * @template M - The type of the metadata included in the response (optional).
  */
-export type ApiSuccess<T> = {
-  /**
-   * Indicates whether the API request was successful.
-   */
-  ok: true;
-  /**
-   * The data returned by the API.
-   */
-  data: T;
-  /**
-   * Optional metadata about the response.
-   */
-  meta?: Record<string, unknown>;
-};
+export type ApiSuccess<T, M = undefined> = M extends undefined
+  ? {
+      ok: true;
+      data: T;
+    }
+  : {
+      ok: true;
+      data: T;
+      meta: M;
+    };
 
 /**
  * Sends a successful API response with a consistent structure.
+ *
+ * @template T - The type of the data returned in the response.
+ * @template S - The HTTP status code (default: 200).
+ * @template M - The type of the metadata included in the response (optional).
+ *
  * @param c The context object.
  * @param data The data to include in the response.
  * @param status The HTTP status code to send (default: 200).
@@ -49,8 +53,13 @@ export type ApiSuccess<T> = {
  */
 export function sendJson<
   T,
-  S extends ContentfulStatusCode,
-  M extends Record<string, unknown>,
+  S extends ContentfulStatusCode = typeof HttpStatus.OK,
+  M extends Record<string, unknown> | undefined = undefined,
 >(c: Context, data: T, status: S = HttpStatus.OK as S, meta?: M) {
-  return c.json<ApiSuccess<T>, S>({ ok: true, data, meta }, status);
+  return c.json<ApiSuccess<T, M>, S>(
+    meta
+      ? ({ ok: true, data, meta } as ApiSuccess<T, M>)
+      : ({ ok: true, data } as ApiSuccess<T, M>),
+    status
+  );
 }
