3f449651c83cf3c37f4fa7a8b71713469c8c00c2
Alexandre Trotel <trotelalexandre@proton.me>
2025-09-04 19:14:59 +0200

    fix: reorder imports for consistency in scalar.router.ts

--- src/routers/scalar.router.ts
+++ src/routers/scalar.router.ts
@@ -1,7 +1,6 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { Scalar } from "@scalar/hono-api-reference";
-import { PREFIX, VERSION } from "@/data/base-path";
-import { OPENAPI_DOC_ROUTE } from "@/data/openapi";
+import { OPENAPI_DOC_ROUTE, PREFIX, VERSION } from "@/data/base-path";
 import type { Env } from "@/zap/utils/env";
 
 export const scalarRouter = new OpenAPIHono<Env>().get(
